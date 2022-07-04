02cfa54c24dcaae8685abdc31026eb5e975815b2
thuyfc <nguyenthuy998lc@gmail.com>
2022-07-04 14:58:56 +0700

    f

--- src/pages/View.js
+++ src/pages/View.js
@@ -15,23 +15,27 @@ const View = () => {
   return (
     < >
 
-<img src="https://pngtree.com/free-backgrounds"
-alt="l"/>
-      <div className=" bg-lime-200 mt-20 w-96 ml-96 px-5 py-5 nav">
+      <div className=" bg-lime-200 mt-20 nav-bar mt-36 ml-96 mr-96 p-5">
         <Link to="/Lists" className="text-center bg-sky-800 rounded-lg  px-2 mbs"> <button>Back list</button></Link>
 
         <h1 className="text-center border-black bg-yellow-800  ">
           Thông tin chi tiết
         </h1>
         <div className="bg-stone-400 border-black pt-2.5">
-          <h1>Tên Sinh viên: {data.state.name} </h1>
-          <h1>Mã Sinh viên: {data.state.codes}</h1>
+         <div className="flex p-5"> <p className="w-12 h-12 bg-lime-200 rounded-full"></p>
+          <div className="pl-2.5">
+         <h1>Tên Sinh viên: {data.state.name} </h1>
+         <h1>Phone : {data.state.phone} </h1>
+         </div>
+         </div>
+         <div className="p-5 ">
+         <h1>Mã Sinh viên: {data.state.codes}</h1>
 
-          <h1>Lớp:{data.state.classs}</h1>
-          <h1>email : {data.state.email} </h1>
-          <h1>Địa chỉ : {data.state.contact} </h1>
-          <h1>Phone : {data.state.phone} </h1>
+<h1>Lớp:{data.state.classs}</h1>
+<h1>email : {data.state.email} </h1>
+<h1>Địa chỉ : {data.state.contact} </h1>
 
+         </div>
         </div>
 
       </div>
